b69999ef0ed021fb1a8e23d6c6de158968244fca
Chris Alfano <chris@jarv.us>
2014-10-12 19:02:50 -0400

    basic cleanup of server

--- mac-server/server.js
+++ mac-server/server.js
@@ -2,55 +2,64 @@ var http = require('http'),
     path = require('path'),
     os = require('os'),
     fs = require('fs'),
-    Busboy = require('busboy'),
-    userData;
+    moment = require('moment'),
+    Busboy = require('busboy');
 
 var app = require('http').createServer(function (req, res) {
-    console.log('received '+req.method+' request:');
-    
-    // Handle Mask ID Request
-    if (req.url == '/maskID') {
+    var userData;
+
+    console.log(moment().format('YYYY-MM-DD HH:mm:ss') + '\t' + req.method + ' ' + req.url);
+
+    // Handle mask ID request
+    if (req.url == '/maskID' && req.method == 'POST') {
         var body = '';
+
         req.on('data', function(data) {
           body += data;
         });
+
         req.on('end', function() {
-        	userData = JSON.parse(body);
+            userData = JSON.parse(body);
             console.log(userData);
-            
+
             // Send word to display back to user
             res.writeHead(200, { 'Content-Type': 'text/plain'});
             res.write('Spill');
             res.end();
         });
-        
-    } else if (req.url == '/omitUser') {
+
+    // Handle OMIT request
+    } else if (req.url == '/omitUser' && req.method == 'POST') {
         console.log('User has just omitted...');
-    
-         res.writeHead(200, {'Content-Type': 'text/plain'});
+
+        res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end();
 
-    // Process File Upload
+    // Handle file upload request
     } else if (req.method == 'POST') {
         var busboy = new Busboy({ headers: req.headers });
-        
+
         busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {      
-            var localPath = path.join('/var/Users/Tyler/Development/imgs/', path.basename(fieldname));
             var saveTo = path.join(os.tmpDir(), path.basename(fieldname));
             file.pipe(fs.createWriteStream(saveTo));
             console.log('Image Uploaded Here: ' + saveTo);
         });
-        
+
         busboy.on('finish', function() {
             res.writeHead(200, { 'Connection': 'close' });
             res.end("That's all folks!");
         });
         return req.pipe(busboy);
-        
+
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end();
+
+    // Handle unrecognized request
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end("Request not recognized");
     }
 });
 
 app.listen(8080);
-console.log('Hello node :)');
+console.log('Lairs of Self mac-server is now listening on port 8080');
