ea7b6e0d9a59defda2f57cfd88762a01f15d1542
Chris Alfano <chris@jarv.us>
2014-10-12 19:37:41 -0400

    refactored server to /omit and /submit stubs

--- mac-server/server.js
+++ mac-server/server.js
@@ -6,60 +6,54 @@ var http = require('http'),
     Busboy = require('busboy');
 
 var app = require('http').createServer(function (req, res) {
-    var userData;
+    console.log('\n' + moment().format('YYYY-MM-DD HH:mm:ss') + '\t' + req.method + ' ' + req.url);
 
-    console.log(moment().format('YYYY-MM-DD HH:mm:ss') + '\t' + req.method + ' ' + req.url);
-
-    // Handle mask ID request
-    if (req.url == '/maskID' && req.method == 'POST') {
-        var body = '';
-
-        req.on('data', function(data) {
-          body += data;
-        });
-
-        req.on('end', function() {
-            userData = JSON.parse(body);
-            console.log(userData);
-
-            // Send word to display back to user
-            res.writeHead(200, { 'Content-Type': 'text/plain'});
-            res.write('Spill');
-            res.end();
-        });
-
-    // Handle OMIT request
-    } else if (req.url == '/omitUser' && req.method == 'POST') {
-        console.log('User has just omitted...');
-
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end();
-
-    // Handle file upload request
-    } else if (req.method == 'POST') {
+    // Handle submit request
+    if (req.url == '/submit' && req.method == 'POST') {
         var busboy = new Busboy({ headers: req.headers });
 
         busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {      
             var saveTo = path.join(os.tmpDir(), path.basename(fieldname));
             file.pipe(fs.createWriteStream(saveTo));
-            console.log('Image Uploaded Here: ' + saveTo);
+            console.log('\tsaved photo: ' + saveTo);
         });
 
         busboy.on('finish', function() {
-            res.writeHead(200, { 'Connection': 'close' });
-            res.end("That's all folks!");
-        });
-        return req.pipe(busboy);
+            res.writeHead(200, {
+                'Connection': 'close',
+                'Content-Type': 'application/json'
+            });
 
+            res.end(JSON.stringify({
+                success: true,
+                password: 'Spill'
+            }));
+
+            // TODO: post station+mask signal to MIDI
+            console.log('\tsent submission signal over MIDI');
+
+            // TODO: post submission to emergence-server
+            console.log('\tposted submission to emergence-server');
+        });
+
+        req.pipe(busboy);
+
+    // Handle omit request
+    } else if (req.url == '/omit' && req.method == 'POST') {
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end();
 
+        // TODO: post omit signal to MIDI
+        console.log('\tsent omission signal over MIDI');
+
     // Handle unrecognized request
     } else {
+        console.log('\trequest not recognized');
+
         res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end("Request not recognized");
+        res.end("request not recognized");
     }
 });
 
 app.listen(8080);
-console.log('Lairs of Self mac-server is now listening on port 8080');
+console.log('\nLairs of Self mac-server is now listening on port 8080\n');
