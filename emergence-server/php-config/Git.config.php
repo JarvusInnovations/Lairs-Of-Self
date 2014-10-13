f5d9e185f0d8b2a55eaca5503ca3878ce2ca6059
Chris Alfano <chris@jarv.us>
2014-10-12 21:17:22 -0400

    initial commit of touch-webapp

--- emergence-server/php-config/Git.config.php
+++ emergence-server/php-config/Git.config.php
@@ -1,6 +1,6 @@
 <?php
 
-Git::$repositories['emergence-server'] = [
+Git::$repositories['jarvus-lairs'] = [
     'remote' => 'git@github.com:JarvusInnovations/Lairs-Of-Self.git',
     'originBranch' => 'master',
     'workingBranch' => 'lairs-of-self.sandbox01.jarv.us',
@@ -9,6 +9,9 @@ Git::$repositories['emergence-server'] = [
         'html-templates' => 'emergence-server/html-templates',
         'php-classes' => 'emergence-server/php-classes',
         'php-config' => 'emergence-server/php-config',
-        'site-root' => 'emergence-server/site-root'
+        'site-root' => 'emergence-server/site-root',
+        'sencha-workspace/.sencha' => ['path' => 'touch-webapp/.sencha', 'localOnly' => false],
+        'sencha-workspace/touch-2.3.1' => ['path' => 'touch-webapp/touch-2.3.1', 'localOnly' => false],
+        'sencha-workspace/LairsOfSelf' => 'touch-webapp/LairsOfSelf'
     ]
 ];
